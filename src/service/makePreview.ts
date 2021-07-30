50cb96a679c9d7821b98eab07c311093fac1c9d5
yaeoni <tjghwnd103@gmail.com>
2021-07-30 21:02:05 +0900

    [FEAT] 더보기 - 인기순 API 개발

---
+++ src/service/makePreview.ts
@@ -0,0 +1,36 @@
+import { db } from '../models';
+import { QueryTypes } from 'sequelize';
+
+import { previewDTO, driveDTO } from '../interface/res/previewDTO';
+
+export function makePreview(result: object[]): previewDTO {
+  let drive: driveDTO[] = [];
+  const preview: previewDTO = {
+    totalCourse: result.length,
+    drive: drive,
+  };
+
+  for (let data of result) {
+    const dateToken = data['date'].split('-');
+
+    const tempDrive: driveDTO = {
+      postId: data['Id'],
+
+      title: data['title'],
+      image: data['image'],
+
+      region: data['region'],
+      theme: data['theme'],
+      warning: data['warning'],
+
+      year: dateToken[0],
+      month: dateToken[1],
+      day: dateToken[2],
+
+      isFavorite: data['isFavorite'] > 0 ? true : false,
+    };
+
+    drive.push(tempDrive);
+  }
+  return preview;
+}
