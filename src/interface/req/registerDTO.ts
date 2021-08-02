cdace3195db07b47eebbcdc95327377abda54db0
yaeoni <tjghwnd103@gmail.com>
2021-08-02 11:48:50 +0900

    [FEAT] 회원가입 위한 user DTO 생성

---
+++ src/interface/req/registerDTO.ts
@@ -0,0 +1,10 @@
+export interface registerDTO {
+  Id: string;
+  password: string;
+  email: string;
+
+  nickname: string;
+  profileImage: string;
+  marketingPush: boolean;
+  marketingEmail: boolean;
+}
