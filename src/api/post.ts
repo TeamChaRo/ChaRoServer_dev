7ce394be9e93db16a01490813aabdeba5c95e0d6
yaeoni <tjghwnd103@gmail.com>
2021-08-09 13:10:11 +0900

    [FIX] 기존 postController -> readController 변경 / 게시물 조회용

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,25 +1,25 @@
 import express from 'express';
 const router = express.Router();
-import { postController } from '../controller';
+import { readController } from '../controller';
 
 /**
  *  @route GET /post/detail/:userEmail/:postId
  *  @desc 게시글 상세정보 조회
  *  @access Public
  */
-router.get('/detail/:userEmail/:postId', postController.readPost);
+router.get('/detail/:userEmail/:postId', readController.readPost);
 
 /**
  *  @route GET /post/like/:userEmail/:postId
  *  @desc 게시글 더보기 조회(인기순)
  *  @access Public
  */
-router.get('/preview/like/:userEmail/:identifier', postController.readLikePreview);
+router.get('/preview/like/:userEmail/:identifier', readController.readLikePreview);
 
 /**
  *  @route GET /post/new/:userEmail/:postId
  *  @desc 게시글 더보기 조회(최신순)
  *  @access Public
  */
-router.get('/preview/new/:userEmail/:identifier', postController.readNewPreview);
+router.get('/preview/new/:userEmail/:identifier', readController.readNewPreview);
 export default router;
