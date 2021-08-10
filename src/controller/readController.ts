d1ffab9e55c1c0e504112aec824cb5cf67f231f3
yaeoni <tjghwnd103@gmail.com>
2021-08-10 13:41:45 +0900

    [FEAT] 메인뷰 비즈니스 로직 정의

--- src/controller/readController.ts
+++ src/controller/readController.ts
@@ -2,9 +2,17 @@ import express, { Request, Response } from 'express';
 
 import { getDetail } from '../service/detailService';
 import { getLikeTrend, getLikeTheme, getLikeLocal, getNewTrend } from '../service/previewService';
+import { getMain } from '../service/mainService';
 // userEmail 검사는.... 따로 빼야되는데, 사실?! 실제로는 이상한 아이디가 들어올 수 없지 않나.
 
-const readPost = async function readPost(req: Request, res: Response) {
+const readMain = async function (req: Request, res: Response) {
+  const { userEmail } = req.params;
+
+  const result = await getMain(userEmail);
+
+  return res.status(result.status).json(result.data);
+};
+const readPost = async function (req: Request, res: Response) {
   const { userEmail, postId } = req.params;
 
   const result = await getDetail(userEmail, postId);
@@ -12,7 +20,7 @@ const readPost = async function readPost(req: Request, res: Response) {
   return res.status(result.status).json(result.data);
 };
 
-const readLikePreview = async function readPost(req: Request, res: Response) {
+const readLikePreview = async function (req: Request, res: Response) {
   const { userEmail, identifier } = req.params;
   const { value } = req.query;
 
@@ -31,7 +39,7 @@ const readLikePreview = async function readPost(req: Request, res: Response) {
   return res.status(result.status).json(result.data);
 };
 
-const readNewPreview = async function readPost(req: Request, res: Response) {
+const readNewPreview = async function (req: Request, res: Response) {
   const { userEmail, identifier } = req.params;
   const { value } = req.query;
 
@@ -51,6 +59,7 @@ const readNewPreview = async function readPost(req: Request, res: Response) {
 };
 
 export default {
+  readMain,
   readPost,
   readLikePreview,
   readNewPreview,
---
+++ src/service/mainService.ts
@@ -0,0 +1,25 @@
+import { db } from '../models';
+import { QueryTypes } from 'sequelize';
+
+import mapping from './mapping.json';
+
+export async function getMain(userEmail: string) {
+  try {
+    return {
+      status: 200,
+      data: {
+        success: true,
+        msg: '오늘은 8월 10일 요 새로운 서버는 연결을 언제하자하는게 좋을까? 눈난뇨',
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      status: 502,
+      data: {
+        success: false,
+        msg: '메인뷰 조회 실패',
+      },
+    };
+  }
+}
