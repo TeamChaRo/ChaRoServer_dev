4b5d66af7c44f92bb5513d34975cd9017c3b34e9
yaeoni <tjghwnd103@gmail.com>
2021-08-12 17:49:10 +0900

    [FIX] 더보기, 검색하기 인기순 / 최신순 정렬 수정

--- src/controller/searchController.ts
+++ src/controller/searchController.ts
@@ -49,7 +49,7 @@ const searchLikePost = async function (req: Request, res: Response) {
   res.status(result.status).json(result.data);
 };
 
-const searchNewPost = async function (req: Request, res: Response): number {
+const searchNewPost = async function (req: Request, res: Response) {
   const { region, theme, warning, userEmail } = req.body;
 
   // 1개의 옵션 선택
@@ -61,7 +61,7 @@ const searchNewPost = async function (req: Request, res: Response): number {
     warning: warning,
     userEmail: userEmail,
   };
-  const result = await getLikeSearch(option, search);
+  const result = await getNewSearch(option, search);
 
   res.status(result.status).json(result.data);
 };
