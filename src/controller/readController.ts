43a03a93613cb6b0e527b07d067f22310bb56361
yaeoni <tjghwnd103@gmail.com>
2021-08-16 13:55:44 +0900

    [FEAT] 인기순 페이지네이션 구현

--- src/controller/readController.ts
+++ src/controller/readController.ts
@@ -5,8 +5,12 @@ import {
   getLikeTrend,
   getLikeTheme,
   getLikeLocal,
-  getNewTrend,
   getLikeMoreTrend,
+  getLikeMoreTheme,
+  getLikeMoreLocal,
+  getNewTrend,
+  getNewTheme,
+  getNewLocal,
 } from '../service/previewService';
 import { getMain } from '../service/mainService';
 // userEmail 검사는.... 따로 빼야되는데, 사실?! 실제로는 이상한 아이디가 들어올 수 없지 않나.
@@ -55,12 +59,12 @@ const readNewPreview = async function (req: Request, res: Response) {
   if (identifier == '0') {
     result = await getNewTrend(userEmail);
   } else if (identifier == '1') {
-    result = await getLikeTheme(userEmail, value as string);
+    result = await getNewTheme(userEmail, value as string);
   } else if (identifier == '2') {
-    result = await getLikeLocal(userEmail, value as string);
+    result = await getNewLocal(userEmail, value as string);
   } else if (identifier == '3') {
     // cumstom_theme 값 ( 관리자 권한 )
-    result = await getLikeTheme(userEmail, 'river');
+    result = await getNewTheme(userEmail, 'river');
   }
   return res.status(result.status).json(result.data);
 };
@@ -76,11 +80,11 @@ const readLikeMorePreview = async function (req: Request, res: Response) {
   if (identifier == '0') {
     result = await getLikeMoreTrend(userEmail, postId, count);
   } else if (identifier == '1') {
-    result = await getLikeTheme(userEmail, value as string);
+    result = await getLikeMoreTheme(userEmail, value as string, postId, count);
   } else if (identifier == '2') {
-    result = await getLikeLocal(userEmail, value as string);
+    result = await getLikeMoreLocal(userEmail, value as string, postId, count);
   } else if (identifier == '3') {
-    // cumstom_theme 값 ( 관리자 권한 )
+    // cumstom_theme 값 ( 관리자 권한 ) <- 얘는 그냥 10개로 놔두자 ㅎ
     result = await getLikeTheme(userEmail, 'river');
   }
   return res.status(result.status).json(result.data);
--- src/service/previewService.ts
+++ src/service/previewService.ts
@@ -64,8 +64,7 @@ export async function getLikeMoreTrend(userEmail: string, postId: string, count:
       status: 200,
       data: {
         success: true,
-        msg:
-          '오늘은 7월 30일 4시 41분,, 저녁시간까진 50분.. 나는 0끼.. 배고파 죽겠다 떡볶이 먹고싶은데 님은 뭐드시고싶으세요?', //"successfully load Today's preview sorted by date",
+        msg: '무한스크롤 성공기념,,, 차로멘.',
         data: makePreview(result),
       },
     };
@@ -103,7 +102,52 @@ export async function getLikeTheme(userEmail: string, theme: string) {
       status: 200,
       data: {
         success: true,
-        msg: '치키차카초코초코초코~ 복덩이 입냄새 ~~ 복덩복덩~', //"successfully load Today's preview sorted by date",
+        msg: '치키차카초코초코초코~ 복덩이 입냄새 ~~ 복덩복덩~',
+        data: makePreview(result),
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      status: 502,
+      data: {
+        success: false,
+        msg: '게시글 더보기 실패',
+      },
+    };
+  }
+}
+
+export async function getLikeMoreTheme(
+  userEmail: string,
+  theme: string,
+  postId: string,
+  count: string
+) {
+  try {
+    const query = `SELECT P.Id, P.title, P.image, P.region, P.theme, P.warning, 
+                      DATE_FORMAT(P.createdAt, '%Y-%m-%d') as date, count(isLike.PreviewId) as isFavorite, count(countLike.PreviewId) as favoriteCount
+                      FROM preview as P
+                      INNER JOIN detail 
+                      LEFT OUTER JOIN likedPost as countLike ON(countLike.PreviewId = P.Id)
+                      LEFT OUTER JOIN likedPost as isLike ON(isLike.PreviewId = P.Id and isLike.UserEmail =:userEmail)
+                      WHERE detail.PostId=P.Id and detail.${theme}= 1
+                      GROUP BY P.Id 
+                      HAVING (count(countLike.PreviewId) = :count and P.Id < :postId)
+                      ORDER BY favoriteCount DESC, P.Id DESC LIMIT 10`;
+
+    const result = await db.sequelize.query(query, {
+      type: QueryTypes.SELECT,
+      replacements: { userEmail: userEmail, count: parseInt(count), postId: parseInt(postId) },
+      raw: true,
+      nest: true,
+    });
+
+    return {
+      status: 200,
+      data: {
+        success: true,
+        msg: '테마 인기순 더보기 무한스크롤 성공 기원',
         data: makePreview(result),
       },
     };
@@ -128,11 +172,62 @@ export async function getLikeLocal(userEmail: string, local: string) {
                         LEFT OUTER JOIN likedPost as countLike ON(countLike.PreviewId = P.Id) 
                         LEFT OUTER JOIN likedPost as isLike ON(isLike.PreviewId = P.Id and isLike.UserEmail =:userEmail)
                         WHERE P.region =:region
-                        GROUP BY P.Id ORDER BY favoriteCount DESC, P.Id DESC LIMIT 10`;
+                        GROUP BY P.Id 
+                        ORDER BY favoriteCount DESC, P.Id DESC LIMIT 10`;
 
     const result = await db.sequelize.query(query, {
       type: QueryTypes.SELECT,
       replacements: { userEmail: userEmail, region: mapping.region[local] },
+      raw: true,
+      nest: true,
+    });
+
+    return {
+      status: 200,
+      data: {
+        success: true,
+        msg: '이 문구를 발견하셨다면 지금 당장 일어나 한승현과 어깨동무하고 털기춤을 추세요', //"successfully load Today's preview sorted by date",
+        data: makePreview(result),
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      status: 502,
+      data: {
+        success: false,
+        msg: '게시글 더보기 실패',
+      },
+    };
+  }
+}
+
+export async function getLikeMoreLocal(
+  userEmail: string,
+  local: string,
+  postId: string,
+  count: string
+) {
+  try {
+    const query = `SELECT P.Id, P.title, P.image, P.region, P.theme, P.warning, 
+                        DATE_FORMAT(P.createdAt, '%Y-%m-%d') as date, count(isLike.PreviewId) as isFavorite, count(countLike.PreviewId) as favoriteCount
+                        FROM preview as P
+                        INNER JOIN detail
+                        LEFT OUTER JOIN likedPost as countLike ON(countLike.PreviewId = P.Id) 
+                        LEFT OUTER JOIN likedPost as isLike ON(isLike.PreviewId = P.Id and isLike.UserEmail =:userEmail)
+                        WHERE P.region =:region
+                        GROUP BY P.Id
+                        HAVING (count(countLike.PreviewId) = :count and P.Id < :postId) 
+                        ORDER BY favoriteCount DESC, P.Id DESC LIMIT 10`;
+
+    const result = await db.sequelize.query(query, {
+      type: QueryTypes.SELECT,
+      replacements: {
+        userEmail: userEmail,
+        region: mapping.region[local],
+        count: parseInt(count),
+        postId: parseInt(postId),
+      },
       raw: true,
       nest: true,
     });
