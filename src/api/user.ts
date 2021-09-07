48f35c7a45fdca691ffc8c8a6408cfba35937da0
yaeoni <tjghwnd103@gmail.com>
2021-09-07 18:58:27 +0900

    [CHORE] 기존 rest 방식 소셜 로그인 api 삭제

--- src/api/user.ts
+++ src/api/user.ts
@@ -48,43 +48,6 @@ router.post(
   loginController.login
 );
 
-/**
- *  @route GET /user/login/google
- *  @desc 구글 소셜 로그인 -> 클라딴에서 해당 URL로 redirect시켜줘야함
- *  @access Public
- */
-router.get('/login/google', function (req, res) {
-  res.redirect(loginController.googleURL);
-});
-
-/**
- *  @route GET /user/login/google/callback
- *  @desc 구글 소셜 로그인 결과 반환(유저 정보 반환)
- *  @access Public
- */
-router.get('/login/google/callback', loginController.googleLogin);
-
-/**
- *  @route GET /user/login/kakao
- *  @desc 카카오 소셜 로그인 -> 클라딴에서 해당 URL로 redirect시켜줘야함
- *  @access Public
- */
-router.get('/login/kakao', function (req, res) {
-  res.redirect(loginController.kakaoURL);
-});
-
-/**
- *  @route GET /user/login/kakao/callback
- *  @desc 카카오 소셜 로그인 결과 반환(유저 정보 반환)
- *  @access Public
- */
-router.get('/login/kakao/callback', loginController.kakaoLogin);
-
-/**
- *  @route GET /user/myPage/like/:userEmail
- *  @desc 마이페이지 정보 반환 - 인기순
- *  @access Public
- */
 router.get('/myPage/like/:userEmail', myPageController.myPageLike);
 
 /**
--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -1,7 +1,4 @@
 import express, { Request, Response } from 'express';
-import axios from 'axios';
-import qs from 'qs';
-import { OAuth2Client } from 'google-auth-library';
 import config from '../config/config';
 
 import loginService from '../service/loginService';
@@ -15,95 +12,6 @@ const login = async function (req: Request, res: Response) {
   res.status(result.status).json(result.data);
 };
 
-/* Google */
-const google = new OAuth2Client(config.googleClientId, config.googleSecret, config.googleRedirect);
-
-const googleURL = google.generateAuthUrl({
-  access_type: 'offline',
-  scope: [
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email',
-  ],
-});
-
-const googleLogin = async function (req: Request, res: Response) {
-  const code = req.query.code;
-  const { tokens } = await google.getToken(code as string);
-  google.setCredentials(tokens);
-
-  const ticket = await google.verifyIdToken({
-    idToken: tokens.id_token,
-    audience: config.googleClientId, // multiple clients면 여러개 client ID 사용 가능
-  });
-  const payload = ticket.getPayload();
-
-  const email = payload.email;
-  const nickname = payload.name;
-
-  console.log('로그인 성공!', email, nickname);
-  res.status(200).json({ success: true, msg: '구글 로그인 성공!' });
-};
-
-/* Kakao */
-const kakao = {
-  clientID: config.kakaoClientId,
-  clientSecret: config.kakaoSecret,
-  redirectUri: config.kakaoRedirect,
-};
-
-const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code`;
-
-const kakaoLogin = async function (req: Request, res: Response) {
-  let token: any, user: any;
-  try {
-    //access토큰을 받기 위한 코드
-    token = await axios({
-      //token
-      method: 'POST',
-      url: 'https://kauth.kakao.com/oauth/token',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-      },
-      data: qs.stringify({
-        grant_type: 'authorization_code', //특정 스트링
-        client_id: kakao.clientID,
-        client_secret: kakao.clientSecret,
-        redirectUri: kakao.redirectUri,
-        code: req.query.code, //결과값을 반환했다. 안됐다.
-      }), //객체를 string 으로 변환
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false, msg: '카카오 토큰 발급 실패' });
-  }
-
-  try {
-    user = await axios({
-      method: 'get',
-      url: 'https://kapi.kakao.com/v2/user/me',
-      headers: {
-        Authorization: `Bearer ${token.data.access_token}`,
-      }, //헤더에 내용을 보고 보내주겠다.
-    });
-
-    // user에게서 필요한 정보
-    const data = user.data;
-    const email = data.kakao_account.email;
-    const nickname = data.properties.nickname;
-
-    console.log('로그인 성공!', email, nickname);
-
-    res.status(200).json({ success: true, msg: '카카오 로그인 성공!' });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false, msg: '카카오 로그인 실패' });
-  }
-};
-
 export default {
-  googleURL,
-  googleLogin,
-  kakaoURL,
-  kakaoLogin,
   login,
 };
