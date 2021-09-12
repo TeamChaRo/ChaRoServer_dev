e45c9ac6889c628b1077e92a00bed5e2d70538d2
yaeoni <tjghwnd103@gmail.com>
2021-09-12 18:03:41 +0900

    [FEAT] DTO 추가 및 적용

--- src/utils/sendMQ.ts
+++ src/utils/sendMQ.ts
@@ -1,8 +1,10 @@
-export default function (key: string, msg: object) {
-  global.ch.assertExchange('direct_logs', 'direct', {
+import { mqDTO } from '../interface/req/mqDTO';
+
+export default function (key: string, msg: mqDTO) {
+  global.ch.assertExchange('direct', 'direct', {
     durable: false,
   });
 
-  global.ch.publish('direct_logs', key, Buffer.from(JSON.stringify(msg)));
+  global.ch.publish('direct', key, Buffer.from(JSON.stringify(msg)));
   console.log('Send: %s', msg);
 }
