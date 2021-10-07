81b7abfc6ff70e9169c926f00bc8c8bd9a402d51
yaeoni <tjghwnd103@gmail.com>
2021-10-07 14:47:33 +0900

    [FEAT] 삭제될 이미지 body로 받는 로직 추가

--- src/controller/postController.ts
+++ src/controller/postController.ts
@@ -3,7 +3,8 @@ import express, { Request, Response } from 'express';
 import { doDelete } from '../service/postService';
 const deletePost = async (req: Request, res: Response) => {
   const { postId } = req.params;
-  const result = await doDelete(postId);
+  const { images } = req.body;
+  const result = await doDelete(postId, images);
   return res.status(result.status).json(result.data);
 };
 
