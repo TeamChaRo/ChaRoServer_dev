b57697f21d406a70921b34350de5446d2a8b0949
yaeoni <tjghwnd103@gmail.com>
2021-10-07 14:50:20 +0900

    [FEAT] 게시글 속 이미지 S3속에서도 삭제토록 로직 추가

--- src/service/postService.ts
+++ src/service/postService.ts
@@ -1,8 +1,36 @@
 import { db } from '../models';
 import { QueryTypes } from 'sequelize';
 
-export async function doDelete(postId: string) {
-  db.Preview.destroy({ where: { Id: postId } });
+import s3 from '../loaders/s3';
+
+export async function doDelete(postId: string, images: string[]) {
+  try {
+    let params = {
+      Bucket: 'charo-release',
+      Delete: { Objects: [] },
+    };
+
+    for (let image of images) {
+      const key = image.split('amazonaws.com/')[1];
+      params.Delete.Objects.push({ Key: key });
+    }
+
+    s3.deleteObjects(params, function (err, data) {
+      if (err) {
+        throw err;
+      }
+    });
+    db.Preview.destroy({ where: { Id: postId } });
+  } catch (err) {
+    console.log(err);
+    return {
+      status: 500,
+      data: {
+        success: false,
+        msg: 'SERVER Error',
+      },
+    };
+  }
 
   return {
     status: 200,
